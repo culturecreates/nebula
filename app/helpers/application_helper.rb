dd9f65c8256cef940075006564bec65f8bee88ba
Gregory Saumier-Finch <gregory@culturecreates.com>
2023-12-30 14:34:09 -0500

    Added page title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,15 @@
 module ApplicationHelper
 
+  # Returns the full title on a per-page basis.
+  def full_title(page_title = '')
+    base_title = "Crawled by Nebula"
+    base_title += " DEV" if Rails.env.development?
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 
   def use_prefix(uri)
 
