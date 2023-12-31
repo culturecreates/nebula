d08cbab4e32a2650bfd4546e515e2db01569c968
Gregory Saumier-Finch <gregory@culturecreates.com>
2023-12-31 14:03:36 -0500

    added graph lists

--- app/controllers/query_controller.rb
+++ app/controllers/query_controller.rb
@@ -1,16 +1,18 @@
 class QueryController < ApplicationController
 
   def show
-    @sparql_file = params[:sparql] ||= "custom/scenepro-orgs"
-    substitute_list = if params[:placeholder] 
-                        ["PLACEHOLDER", params[:placeholder]]
-                      else 
-                        []
-                      end
+    sparql_file = params[:sparql] ||= "custom/scenepro-orgs"
+    sub_sparql = params[:sub_sparql] ||= "list_entities"
+    @title =  params[:title] ||=  params[:sparql]
+
+    # create list for SPARQL template
+    substitute_list = []
+    ["PLACEHOLDER", params[:placeholder]].each { |item| substitute_list << item } if params[:placeholder] 
     ["API", params[:api]].each { |item| substitute_list << item } if params[:api]
+    ["SUB_SPARQL", sub_sparql].each { |item| substitute_list << item }
 
     sparql = SPARQL::Client.new("http://db.artsdata.ca/repositories/artsdata")
-    query =  SparqlLoader.load(@sparql_file, substitute_list)
+    query =  SparqlLoader.load(sparql_file, substitute_list)
     @solutions = sparql.query(query).limit(1000)
   end
 end
