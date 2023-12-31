102e1f2e69a606b1e6667468aaad3d3565c01739
Gregory Saumier-Finch <gregory@culturecreates.com>
2023-12-30 21:53:00 -0500

    added generic link to view entities

--- app/controllers/query_controller.rb
+++ app/controllers/query_controller.rb
@@ -2,8 +2,15 @@ class QueryController < ApplicationController
 
   def show
     @sparql_file = params[:sparql] ||= "custom/scenepro-orgs"
+    substitute_list = if params[:placeholder] 
+                        ["PLACEHOLDER", params[:placeholder]]
+                      else 
+                        []
+                      end
+    ["API", params[:api]].each { |item| substitute_list << item } if params[:api]
+
     sparql = SPARQL::Client.new("http://db.artsdata.ca/repositories/artsdata")
-    query =  SparqlLoader.load(@sparql_file)
+    query =  SparqlLoader.load(@sparql_file, substitute_list)
     @solutions = sparql.query(query).limit(1000)
   end
 end
