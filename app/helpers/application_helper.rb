4f689762b97580667a6da37fce492173dc4b0ba8
Gregory Saumier-Finch <gregory@culturecreates.com>
2024-01-03 16:11:48 -0500

    Tab title set to Artsdata

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = "Crawled by Nebula"
+    base_title = "Artsdata"
     base_title += " DEV" if Rails.env.development?
     if page_title.empty?
       base_title
