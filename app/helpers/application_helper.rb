3d03da41ce4c1cacac45989d73f3ddcbd66b32c0
Gregory Saumier-Finch <gregory@culturecreates.com>
2024-01-09 10:43:54 -0500

    added artsdata prefix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,10 +19,10 @@ module ApplicationHelper
 
     
     uri_compact = uri.gsub("http://schema.org/","schema:")
-      .gsub("http://kg.artsdata.ca/resource/","")
+      .gsub("http://kg.artsdata.ca/resource/","artsdata:")
       .gsub("http://kg.footlight.io/resource/","footlight-console:")
-      .gsub("http://api.footlight.io/places/","footlight-places:")
-      .gsub("http://api.footlight.io/events/","footlight-events:")
+      .gsub("http://api.footlight.io/places/","footlight-cms-places:")
+      .gsub("http://api.footlight.io/events/","footlight-cms-events:")
       .gsub("http://www.w3.org/2000/01/rdf-schema#","rdfs:")
       .gsub("http://www.w3.org/2002/07/owl#","owl:")
       .gsub("http://www.w3.org/2004/02/skos/core#","skos:")
