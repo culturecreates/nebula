65516f5fc7baf716abaea0fdf4672552e54bfc15
Gregory Saumier-Finch <gregory@culturecreates.com>
2024-01-18 17:46:28 -0500

    first minter for scenepro places

--- app/controllers/mint_controller.rb
+++ app/controllers/mint_controller.rb
@@ -1,5 +1,4 @@
 class MintController < ApplicationController
-
   before_action :authenticate_user! # ensure user is logged in
 
   # show tools to mint a new Artsdata URI
--- app/javascript/controllers/artsdata_controller.js
+++ app/javascript/controllers/artsdata_controller.js
@@ -1,18 +1,32 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "uri","button" ]
+  static targets = ["uri", "button" ]
+  static values = { externaluri: String, classtomint: String, authority: String}
 
-  
+  connect() {
+    console.log(this.externaluriValue); // logs the value of @your_variable
+  }
 
   async mintEntity() {
-    this.buttonTarget.hidden = true
-    const url = "https://api.artsdata.ca/resource.json?uri=K12-382"
-    const res = await fetch(url);
+    this.buttonTarget.disabled = true
+    const url = 'https://api.artsdata.ca/mint';
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        "externalUri": this.externaluriValue,
+        "classToMint": this.classtomintValue,
+        "publisher": this.authorityValue
+      })
+    }
+    const res = await fetch(url, options);
     const json = await res.json();
-    console.log(this.buttonTargets);
     console.log(json);
-    this.uriTarget.innerHTML = json.length + " Results" ;
+    this.uriTarget.innerHTML = JSON.stringify(json, null, 2);
+
   }
 
   
