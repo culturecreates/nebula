f525f946b099b2c79920ba23de78aa36fdcda85d
Gregory Saumier-Finch <gregory@culturecreates.com>
2024-02-06 19:58:23 -0500

    added daisy chained sparqls

--- app/controllers/query_controller.rb
+++ app/controllers/query_controller.rb
@@ -15,4 +15,26 @@ class QueryController < ApplicationController
     query =  SparqlLoader.load(sparql_file, substitute_list)
     @solutions = sparql.query(query).limit(1000)
   end
+
+  # pass a chain of SPARQL queries to the SPARQL endpoint
+  # GET "query/show_chain"
+  def show_chain
+    construct_files = params[:constructs].split(",")
+    puts construct_files
+    query_file = params[:query]
+    @title =  params[:title] ||=  query
+
+    graph = RDF::Graph.new
+    sparql_client = SPARQL::Client.new("http://db.artsdata.ca/repositories/artsdata")
+    construct_files.each do |sparql_file|
+      query =  SparqlLoader.load(sparql_file)
+      graph << sparql_client.query(query)
+      puts "query #{sparql_file} added #{graph.count} triples"
+    end
+    # apply final query to local graph
+    query =  SparqlLoader.load(query_file)
+    @solutions =  SPARQL.execute(query,graph)
+
+    render :show
+  end
 end
