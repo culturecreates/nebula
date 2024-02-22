23258da2319d76f8aee90a9cc774b454f85947fb
Gregory Saumier-Finch <gregory@culturecreates.com>
2024-02-21 21:45:12 -0500

    added i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
 
   def home; end
 
@@ -8,5 +9,9 @@ class ApplicationController < ActionController::Base
     return true if session[:name] || Rails.env.test?
     flash.alert = "You must be logged in to access this section"
     redirect_back(fallback_location: root_path)
+  end
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,24 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  root "application#home"
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+    # Defines the root path route ("/")
+    root "application#home"
 
-  get "test", to: "application#test"
+    get "test", to: "application#test"
 
-  get "entity", to: "entity#show"
-  get "entity/unsupported_claims", to: "entity#unsupported_claims"
-  get "entity/derived_statements", to: "entity#derived_statements"
-  get "entity/expand", to: "entity#expand"
-  get "dereference/card", to: "dereference#card"
-  get "dereference/external", to: "dereference#external"
-  get "github/callback", to: "github#callback"
-  get "query/show", to: "query#show"
-  get "mint/preview", to: "mint#preview"
-  get "mint/link", to: "mint#link"
-  get "reconcile/query", to: "reconcile#query"
+    get "entity", to: "entity#show"
+    get "entity/unsupported_claims", to: "entity#unsupported_claims"
+    get "entity/derived_statements", to: "entity#derived_statements"
+    get "entity/expand", to: "entity#expand"
+    get "dereference/card", to: "dereference#card"
+    get "dereference/external", to: "dereference#external"
+    get "github/callback", to: "github#callback"
+    get "query/show", to: "query#show"
+    get "mint/preview", to: "mint#preview"
+    get "mint/link", to: "mint#link"
+    get "reconcile/query", to: "reconcile#query"
+  end
 
   get 'sparql', to: redirect('http://artsdata-trifid-production.herokuapp.com/sparql/')
 
