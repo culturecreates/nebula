1c77fb9098d2ab4df59c6934ec1dc7453043f389
Gregory Saumier-Finch <gregory@culturecreates.com>
2024-02-24 19:01:42 -0500

    removed github routes from i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
     I18n.locale = params[:locale] || I18n.default_locale
   end
 
-  def default_url_options
-    { locale: I18n.locale }
-  end
+  # def default_url_options
+  #   { locale: I18n.locale }
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,6 @@ Rails.application.routes.draw do
     get "entity/expand", to: "entity#expand"
     get "dereference/card", to: "dereference#card"
     get "dereference/external", to: "dereference#external"
-    get "github/callback", to: "github#callback"
     get "query/show", to: "query#show"
     get "mint/preview", to: "mint#preview"
     get "mint/link", to: "mint#link"
@@ -21,7 +20,7 @@ Rails.application.routes.draw do
   end
 
   get 'sparql', to: redirect('http://artsdata-trifid-production.herokuapp.com/sparql/')
-
   get "resource/:id", to: "resource#show"
+  get "github/callback", to: "github#callback"
 
 end
