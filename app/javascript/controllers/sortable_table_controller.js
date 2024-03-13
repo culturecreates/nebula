92a64b530dddd381ad2c8640b93803c6d13002f1
Gregory Saumier-Finch <gregory@culturecreates.com>
2024-03-12 20:00:04 -0400

    fix sorting columns by number when float or integer

--- app/javascript/controllers/sortable_table_controller.js
+++ app/javascript/controllers/sortable_table_controller.js
@@ -24,8 +24,10 @@ export default class extends Controller {
       const cellB = rowB.cells[index].textContent.trim()
     
       // Check if the cells are numbers
-      if (!isNaN(cellA) && !isNaN(cellB)) {
-        return parseFloat(cellA) - parseFloat(cellB)
+      if ((!isNaN(cellA) && !isNaN(cellB)) || 
+      (cellA.match(/xsd:integer/) && cellB.match(/xsd:integer/)) ||
+      (cellA.match(/xsd:float/) && cellB.match(/xsd:float/)) ) {
+        return parseFloat(cellA.replace("xsd:integer", "").replace("xsd:float", "")) - parseFloat(cellB.replace("xsd:integer", "").replace("xsd:float", ""))
       } else {
         return cellA.localeCompare(cellB)
       }
