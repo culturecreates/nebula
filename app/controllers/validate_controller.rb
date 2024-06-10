121b415216f7f797f5b1715f1f88840286387f9a
Gregory Saumier-Finch <gregory@culturecreates.com>
2024-06-10 11:47:25 -0400

    added validation for person coming from Wikidata

--- app/controllers/validate_controller.rb
+++ app/controllers/validate_controller.rb
@@ -3,7 +3,11 @@ class ValidateController < ApplicationController
   def show
     uri = params[:uri] 
     @entity = Entity.new(entity_uri: uri)
-    @entity.load_graph
+    wikidata_sparql_endpoint= "https://query.wikidata.org/sparql"
+    wikidata_sparql = SPARQL::Client.new(wikidata_sparql_endpoint)
+    response = wikidata_sparql.query("CONSTRUCT { <#{uri}> a schema:Person; schema:name ?name ; schema:sameAs ?aduri . } WHERE { <#{uri}> ?p ?o ; <http://www.wikidata.org/prop/direct/P7627> ?adid . BIND (URI(CONCAT(\"http://kg.artsdata.ca/resource/\",?adid)) as ?aduri) }")
+    @entity.graph = RDF::Graph.new << response 
+
     begin
       shacl = SHACL.open("app/services/shacls/mint_person.ttl")
       @report = shacl.execute(@entity.graph)
@@ -13,4 +17,6 @@ class ValidateController < ApplicationController
     end
    
   end
+
+
 end
