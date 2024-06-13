2b4f82d8be95707c4f4b591dccefc664703a7aee
Gregory Saumier-Finch <gregory@culturecreates.com>
2024-06-13 13:44:55 -0400

    add organization minting from Wikidata

--- app/controllers/validate_controller.rb
+++ app/controllers/validate_controller.rb
@@ -3,50 +3,16 @@ class ValidateController < ApplicationController
   def show
     uri = params[:uri] 
     @entity = Entity.new(entity_uri: uri)
+    @class_to_mint = params[:class_to_mint] # i.e. schema:Person
+
     wikidata_sparql_endpoint= "https://query.wikidata.org/sparql"
     wikidata_sparql = SPARQL::Client.new(wikidata_sparql_endpoint)
-    response = wikidata_sparql.query(<<-SPARQL)
-      CONSTRUCT { 
-        <#{uri}> a schema:Person ; 
-        schema:name ?name ; 
-        schema:sameAs ?aduri ;
-        schema:description ?desc ;
-        schema:gender ?gender ;
-        schema:occupation ?occupation ;
-        schema:birthDate ?birth_date ;
-        schema:birthPlace ?birth_place ;
-        schema:url ?url ;
-        schema:sameAs ?isniuri , ?music_brainz .
-      } 
-      WHERE { 
-        <#{uri}> <http://www.w3.org/2000/01/rdf-schema#label> ?name . 
-        filter (lang(?name) = 'en' || lang(?name) = 'fr')
 
-        OPTIONAL { 
-          <#{uri}>  schema:description ?desc . 
-          filter (lang(?desc) = 'en' || lang(?desc) = 'fr') 
-        }
-          
-        OPTIONAL { 
-          <#{uri}> <http://www.wikidata.org/prop/direct/P7627> ?adid  .
-          BIND (URI(CONCAT(\"http://kg.artsdata.ca/resource/\",?adid)) as ?aduri) 
-        }
-        OPTIONAL { <#{uri}> <http://www.wikidata.org/prop/direct/P21> ?gender .}
-        OPTIONAL { <#{uri}> <http://www.wikidata.org/prop/direct/P106> ?occupation .}
-        OPTIONAL { <#{uri}> <http://www.wikidata.org/prop/direct/P19> ?birth_place .}
-        OPTIONAL { <#{uri}> <http://www.wikidata.org/prop/direct/P569> ?birth_date .}
-        OPTIONAL { <#{uri}> <http://www.wikidata.org/prop/direct/P856> ?url .}
-        OPTIONAL { 
-          <#{uri}> <http://www.wikidata.org/prop/direct/P213> ?isni .
-          BIND (URI(CONCAT(\"https://isni.org/isni/\",?isni)) as ?isniuri)
-        }
-        OPTIONAL { <#{uri}> <http://www.wikidata.org/prop/direct-normalized/P434> ?music_brainz .}
-      }
-    SPARQL
-      @entity.graph = RDF::Graph.new << response 
+    response = wikidata_sparql.query(sparql_by_class_to_mint(@class_to_mint, uri))
+    @entity.graph = RDF::Graph.new << response 
 
     begin
-      shacl = SHACL.open("app/services/shacls/mint_person.ttl")
+      shacl = SHACL.open(shacl_by_class_to_mint(@class_to_mint))
       @report = shacl.execute(@entity.graph)
       @entity.load_graph_into_graph(@report)
     rescue => exception
@@ -54,6 +20,26 @@ class ValidateController < ApplicationController
     end
    
   end
+
+  def sparql_by_class_to_mint(class_to_mint, uri)
+    case class_to_mint
+    when "schema:Person"
+      SparqlLoader.load("validate_controller/external_person_data", ["EXTERNAL_URI", uri])
+    when "schema:Organization"
+      SparqlLoader.load("validate_controller/external_organization_data", ["EXTERNAL_URI", uri])
+    end
+  end
+
+  def shacl_by_class_to_mint(class_to_mint)
+    case class_to_mint
+    when "schema:Person"
+      "app/services/shacls/mint_person.ttl"
+    when "schema:Organization"
+      "app/services/shacls/mint_organization.ttl"
+    end
+
+  end
+
 
 
 end
