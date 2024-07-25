dc574c737c2bc9fa2646cacd488a1df444625c02
Gregory Saumier-Finch <gregory@culturecreates.com>
2024-07-25 17:52:38 -0400

    made potentialAction dynamic

--- app/javascript/controllers/githubapi_controller.js
+++ app/javascript/controllers/githubapi_controller.js
@@ -4,41 +4,50 @@ export default class extends Controller {
   static targets = ["result", "button" ]
   static values = { 
     token: String, 
-    url: String
+    url: String,
+    method: String
   }
 
   connect() {
     console.log("token", this.tokenValue)
     console.log("url", this.urlValue)
+    console.log("httpMethod", this.methodValue)
   }
 
   async runAction() {
     this.buttonTarget.disabled = true
-   // const url = this.urlValue;
-   const url = "https://api.github.com/repos/culturecreates/artsdata-google-workspace-smart-chip/actions/workflows/push-to-artsdata.yml/dispatches"
+    const url = this.urlValue;
+    // const url = "https://api.github.com/repos/culturecreates/artsdata-google-workspace-smart-chip/actions/workflows/push-to-artsdata.yml/dispatches"
     const options = {
-      method: 'POST',
+      method: this.methodValue,
       headers: {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer ' + this.tokenValue,
         'Accept': 'application/vnd.github+json',
         'X-GitHub-Api-Version': '2022-11-28'
-      },
-      body: JSON.stringify({
+      }
+    }
+    if (this.methodValue == "POST") {
+      options.body = JSON.stringify({
         'ref': 'main'
       })
     }
-    const res = await fetch(url, options);
-    console.log("res:", res);
-    if (res.status == 204) {
-      this.resultTarget.innerHTML = "Successfully ran the action. View all <a href='https://github.com/culturecreates/artsdata-google-workspace-smart-chip/actions'>workflows</a>.";
-    } else {
-      const json = await res.json();
-
-      this.resultTarget.innerHTML = "Error: " + JSON.stringify(json, null, 2);
+    try {
+      const res = await fetch(url, options);
+      if (!res.ok) {
+        throw new Error(`HTTP error! status: ${res.status}`);
+      }
+      console.log("res:", res);
+      if (res.status == 204) {
+        this.resultTarget.innerHTML = "Successfully ran the action. Check the GitHub Actions tab for the status.";
+      } else {
+        const json = await res.json();
+        this.resultTarget.innerHTML = "Error: " + JSON.stringify(json, null, 2);
+      }
+    } catch (error) {
+      console.error('Fetch error:', error);
+      // Handle the error appropriately here
     }
-
-
   }
 
   
