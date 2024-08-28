418fa3a8e833ab4efc85a4161dea60c0ffa3bfc8
Gregory Saumier-Finch <gregory@culturecreates.com>
2024-08-28 18:06:24 -0400

    Run VCR in test and dev only

--- config/initializers/vcr.rb
+++ config/initializers/vcr.rb
@@ -1,5 +1,7 @@
-VCR.configure do |config|
-  # config.cassette_library_dir = "spec/cassettes"
-  # config.hook_into :webmock
-  config.allow_http_connections_when_no_cassette = true
+if Rails.env.development? || Rails.env.test?
+  VCR.configure do |config|
+    # config.cassette_library_dir = "spec/cassettes"
+    # config.hook_into :webmock
+    config.allow_http_connections_when_no_cassette = true
+  end
 end
