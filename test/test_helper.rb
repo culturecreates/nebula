73ea150bc818d2aee4eca04b7e15cfbfda48a2b2
Gregory Saumier-Finch <gregory@culturecreates.com>
2024-08-28 20:20:15 -0400

    fix for VCR configure

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,6 +11,7 @@ require 'webmock/minitest'
 
 
 VCR.configure do |config|
+  config.allow_http_connections_when_no_cassette = true
   config.cassette_library_dir = "test/vcr_cassettes"
   config.hook_into :webmock
 end
