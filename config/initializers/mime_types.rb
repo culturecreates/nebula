50c1896c7b298143925d9c92b3a2c7446bd0182b
Gregory Saumier-Finch <gregory@culturecreates.com>
2024-08-29 09:36:49 -0400

    added ld+json to MIME types for files in public folder

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -3,3 +3,6 @@ Mime::Type.register "application/star+ld+json", :jsonlds
 Mime::Type.register "text/turtle", :ttl
 Mime::Type.register "text/star+turtle", :ttls
 Mime::Type.register "application/rdf+json", :rdf
+
+# Public files
+Rack::Mime::MIME_TYPES[".jsonld"]="application/ld+json"
