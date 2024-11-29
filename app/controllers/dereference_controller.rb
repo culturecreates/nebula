902c8b359f3b07969b04c08ab8281f58779ec461
Gregory Saumier-Finch <gregory@culturecreates.com>
2024-11-29 16:23:33 -0500

    allow any github user to dereference and dereference blank nodes

--- app/controllers/dereference_controller.rb
+++ app/controllers/dereference_controller.rb
@@ -1,6 +1,6 @@
 class DereferenceController < ApplicationController
   rescue_from StandardError, with: :failed_dereference
-  before_action :authenticate_user!, only: [:external] # ensure user has permissions
+  before_action :user_signed_in?, only: [:external] # ensure user has permissions
 
   # /dereference/card?uri=
   def card
@@ -18,20 +18,14 @@ class DereferenceController < ApplicationController
     @max_entities_per_page = 40
     @entity = Entity.new(entity_uri:params[:uri])
     @entity.dereference
-    @entity.replace_blank_subject_nodes
-    @entity.replace_blank_nodes # first level
-    @entity.replace_blank_nodes # second level
-    @entity.replace_blank_nodes # third level
     
     shacl = SHACL.open(@shacl_url)
     @report = shacl.execute(@entity.graph)
     @entity.load_graph_into_graph(@report)
-
     # SPARQL to run after dereferencing (post dereferce)
     if @post_sparql
       sparql_url = @post_sparql
       @entity.construct_sparql(sparql_url)
-      @entity.replace_blank_nodes # first level
     end
     respond_to do |format|
       format.jsonld {
