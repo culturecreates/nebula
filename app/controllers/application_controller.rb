4f7af71504dafabfd20b8cdd6f4e91ff9257fd22
Gregory Saumier-Finch <gregory@culturecreates.com>
2024-11-29 17:52:59 -0500

    Only allow dereferencing if signed in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,33 +7,43 @@ class ApplicationController < ActionController::Base
   def test; end
 
   def user_signed_in?
-    session[:handle].present?
+    if session[:handle].present?
+      return true
+    else
+     return false
+    end
+  end
+
+  def user_signed_in!
+    if !user_signed_in?
+      flash.alert = "You must be logged in to access this section"
+      redirect_back(fallback_location: root_path)
+    end
   end
 
   def authenticate_user!
     return true if  Rails.env.test?
 
-    if session[:handle]
-      # TODO: Replace with user table
-      if ["dlh28",
-        "MorganPann",
-        "GaryMan1968",
-        "fjjulien",
-        "saumier",
-        "Liverace",
-        "sahalali",
-        "tammy-culture",
-        "MichifDorian",
-        "troughc"].include?(session[:handle])
-        return true
-      else
-        flash.alert = "#{session[:name]} does not have sufficient permissions to access this section."
-        redirect_back(fallback_location: root_path)
-      end
+    # Redirect if not signed in
+    user_signed_in!
+
+    # TODO: Replace with user table
+    if ["dlh28",
+      "MorganPann",
+      "GaryMan1968",
+      "fjjulien",
+      "saumier",
+      "Liverace",
+      "sahalali",
+      "tammy-culture",
+      "MichifDorian",
+      "troughc"].include?(session[:handle])
+      return true
     else
-    flash.alert = "You must be logged in to access this section"
-    redirect_back(fallback_location: root_path)
+      flash.alert = "#{session[:name]} does not have sufficient permissions to access this section."
+      redirect_back(fallback_location: root_path)
     end
+   
   end
 
   def set_locale
