2a2318022db0f75f1094dac338a1cf847fb6afd7
Gregory Saumier-Finch <gregory@culturecreates.com>
2025-02-05 17:02:10 -0500

    changed ical url construction

--- app/controllers/ical_controller.rb
+++ app/controllers/ical_controller.rb
@@ -7,7 +7,7 @@ class IcalController < ApplicationController
     @icals.each do |ical|
       ical[:jsonld_url] = "https://api.artsdata.ca/query?format=jsonld&sparql=#{ical["download_url"]}"
       ical[:dereference_url] = dereference_external_url(uri: ical[:jsonld_url])
-      ical[:feed_url] = "https://artsdata-ical-2b5d78f4cfc6.herokuapp.com/conversion/convert?url=" + URI.encode_www_form_component(ical[:jsonld_url])
+      ical[:feed_url] = "https://ical.artsdata.ca/conversion/convert?url=" + URI.encode_www_form_component(ical[:jsonld_url])
     end
   end
   
