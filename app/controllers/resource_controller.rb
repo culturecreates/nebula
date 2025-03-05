101900d91a00f49c52f28b60913c19ce7b6b0ced
Gregory Saumier-Finch <gregory@culturecreates.com>
2025-03-05 10:16:09 -0500

    force URI dereferencing to 303 see other using HTTPS secure SSL

--- app/controllers/resource_controller.rb
+++ app/controllers/resource_controller.rb
@@ -6,7 +6,7 @@ class ResourceController < ApplicationController
   # Use request header 'Accept'
   # TODO: Try to replace this with rack/content_netgotiation
   def show
-    uri = "http://kg.artsdata.ca" + request.path # allow testing on domains like localhost
+    uri = "https://kg.artsdata.ca" + request.path # allow testing on domains like localhost
     format =  if request.headers['Accept'].include?('application/rdf+xml')
                 :rdf
               elsif request.headers['Accept'].include?('application/ld+json')
