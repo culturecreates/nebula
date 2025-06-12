40a5672f959aa20a6e264350ab14bb54e03187d5
Gregory Saumier-Finch <gregory@culturecreates.com>
2025-06-12 13:54:02 -0400

    when reconciling a Person or Organization switch to dbo:Agent

--- app/helpers/mint_helper.rb
+++ app/helpers/mint_helper.rb
@@ -1,2 +1,11 @@
 module MintHelper
+
+  def classToReconcile(classToMint)
+    if classToMint.value == "http://schema.org/Person" || classToMint.value == "http://schema.org/Organization"
+      "dbo:Agent"
+    else
+      classToMint
+    end
+  end
+
 end
