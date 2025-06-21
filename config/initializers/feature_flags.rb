5227b57bf8527f226bdfb46e484bc8486bf0169c
Gregory Saumier-Finch <gregory@culturecreates.com>
2025-06-21 16:52:30 -0400

    turn on minting feature flag

--- config/initializers/feature_flags.rb
+++ config/initializers/feature_flags.rb
@@ -1,2 +1,2 @@
-Rails.application.config.feature_minting_enabled = false
+Rails.application.config.feature_minting_enabled = true
 Rails.application.config.feature_maintenance_mode_enabled = false # set to false for normal operation
