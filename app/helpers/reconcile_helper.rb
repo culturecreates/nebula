59951cc57d3304f9cf98c01ea67fde88a77b8d88
Gregory Saumier-Finch <gregory@culturecreates.com>
2025-07-21 11:52:33 -0400

    chore: integrate compiled react into rails view

--- app/helpers/reconcile_helper.rb
+++ app/helpers/reconcile_helper.rb
@@ -1,2 +1,14 @@
 module ReconcileHelper
+  def vite_asset_path(asset_name)
+    manifest_path = Rails.root.join('public', 'batch-reconciliation-ui', '.vite','manifest.json')
+   
+    manifest = JSON.parse(File.read(manifest_path))
+    puts "Manifest: #{manifest}"
+    if asset_name == 'index.js'
+      "/batch-reconciliation-ui/#{manifest.dig('index.html', 'file')}"
+    elsif asset_name == 'index.css'
+      "/batch-reconciliation-ui/#{manifest.dig('index.html', 'css').first}"
+    end
+
+  end
 end
