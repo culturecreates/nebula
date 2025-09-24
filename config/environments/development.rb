ede63ff7294620c293834b3f935bf8c8b9f48807
Gregory Saumier-Finch <gregory@culturecreates.com>
2025-09-24 10:17:53 -0400

    support both versions config.artsdata_recon_endpoint_v0

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,6 +7,7 @@ Rails.application.configure do
   prefix = "staging." # Set to "staging." for staging or "" for production
   config.graph_api_endpoint = "http://#{prefix}db.artsdata.ca" 
   config.artsdata_recon_endpoint = "https://#{prefix}recon.artsdata.ca" 
+  config.artsdata_recon_endpoint_v0 = "https://#{prefix}api.artsdata.ca/recon"
   config.artsdata_mint_endpoint = "https://#{prefix}api.artsdata.ca/mint" 
   config.artsdata_link_endpoint = "https://#{prefix}api.artsdata.ca/link"
   config.artsdata_databus_endpoint = "https://#{prefix}api.artsdata.ca/databus"
