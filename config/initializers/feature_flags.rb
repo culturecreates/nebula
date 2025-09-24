61c13612e1e9c80da6e2a2700408ff04089c7c4d
Gregory Saumier-Finch <gregory@culturecreates.com>
2025-09-24 12:41:33 -0400

    news flash for batch reconcile

--- config/initializers/feature_flags.rb
+++ config/initializers/feature_flags.rb
@@ -1,6 +1,6 @@
 
-Rails.application.config.feature_minting_enabled = false
-Rails.application.config.feature_maintenance_mode_enabled = true # set to false for normal operation
-Rails.application.config.announcement_enabled = false
-Rails.application.config.announcement_message = "News flash! <a href=/entity?uri=http%3A%2F%2Fkg.artsdata.ca%2Fresource%2FArtsdataEventTypes>Artsdata Event Types</a> v4.0 now available!".html_safe
-Rails.application.config.announcement_start_time = Time.now.to_i
+Rails.application.config.feature_minting_enabled = true
+Rails.application.config.feature_maintenance_mode_enabled = false # set to false for normal operation
+Rails.application.config.announcement_enabled = true
+Rails.application.config.announcement_message = "News flash! <a href=/query/show?sparql=reconcile_controller/graphs_with_agents_to_reconcile>Batch Reconcile</a> for People, Organizations and Places is now available!".html_safe
+Rails.application.config.announcement_start_time = 1758728904
