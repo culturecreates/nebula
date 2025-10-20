81e926bb87c2e50e2788a9e9073b275e3079f3e1
Gregory Saumier-Finch <gregory@culturecreates.com>
2025-10-20 10:21:50 -0400

    added CMS v4 view

--- app/controllers/footlight_controller.rb
+++ app/controllers/footlight_controller.rb
@@ -14,17 +14,16 @@ class FootlightController < ApplicationController
     # source = "http://kg.artsdata.ca/culture-creates/artsdata-planet-ville-de-laval/calendrier-activites"
     version = params[:version] || "v3"
     case version
-    when "v2"
-      redirect_to dereference_external_path(uri: cms_link_v2(uri, source)), status: 303, allow_other_host: true
     when "v3"
       redirect_to dereference_external_path(uri: cms_link_v3(uri, source)), status: 303, allow_other_host: true
+    when "v4"
+      redirect_to dereference_external_path(uri: cms_link_v4(uri, source)), status: 303, allow_other_host: true
     end 
-
   end
 
 
-  def cms_link_v2(uri, source)
-    "https://api.artsdata.ca/query?frame=event_footlight&format=jsonld&sparql=https://raw.githubusercontent.com/culturecreates/footlight-aggregator/main/sparql/query-event-uri-v2.sparql&uri=#{CGI.escape(uri)}&source=#{CGI.escape(source)}"
+  def cms_link_v4(uri, source)
+    "https://api.artsdata.ca/query?frame=event_footlight&format=jsonld&sparql=https://raw.githubusercontent.com/culturecreates/footlight-aggregator/main/sparql/query-event-uri-v4.sparql&uri=#{CGI.escape(uri)}&source=#{CGI.escape(source)}"
   end
 
   def cms_link_v3(uri, source)
