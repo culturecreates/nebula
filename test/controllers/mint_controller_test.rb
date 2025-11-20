767bf5fa8d2321227ae9b07a788a99dd7fcf7df9
Gregory Saumier-Finch <gregory@culturecreates.com>
2025-11-20 12:49:05 -0500

    unti test not passing - should not merge

--- test/controllers/mint_controller_test.rb
+++ test/controllers/mint_controller_test.rb
@@ -4,7 +4,7 @@ class MintControllerTest < ActionDispatch::IntegrationTest
   
   test "should redirect to root with missing externalUri param in API call" do
     get mint_preview_url, params: { classToMint: 'SomeClass' }
-    assert_equal 'Missing a required param. Required list: [:externalUri]', flash[:alert]
+    assert_equal 'fffgffggMissing a required param. Required list: [:externalUri]', flash[:alert]
   end
   
 end
