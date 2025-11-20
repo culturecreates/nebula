e520a5f9fbd4346af56c0cd963d8fb8dbb3e8fa0
Gregory Saumier-Finch <gregory@culturecreates.com>
2025-11-20 11:57:00 -0500

    update test message

--- test/controllers/mint_controller_test.rb
+++ test/controllers/mint_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class MintControllerTest < ActionDispatch::IntegrationTest
   
-  test "should redirect to root with missing externalUri param" do
+  test "should redirect to root with missing externalUri param in API call" do
     get mint_preview_url, params: { classToMint: 'SomeClass' }
     assert_equal 'Missing a required param. Required list: [:externalUri]', flash[:alert]
   end
