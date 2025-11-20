250c10b6641b0b9de3e70595091269fda557761c
Gregory Saumier-Finch <gregory@culturecreates.com>
2025-11-20 11:47:24 -0500

    test ruleset to require tests to pass

--- test/controllers/mint_controller_test.rb
+++ test/controllers/mint_controller_test.rb
@@ -2,9 +2,9 @@ require "test_helper"
 
 class MintControllerTest < ActionDispatch::IntegrationTest
   
-  test "should redirect to root with missing externalUri" do
+  test "should redirect to root with missing externalUri param" do
     get mint_preview_url, params: { classToMint: 'SomeClass' }
-    assert_equal 'rrrrrMissing a required param. Required list: [:externalUri]', flash[:alert]
+    assert_equal 'Missing a required param. Required list: [:externalUri]', flash[:alert]
   end
   
 end
