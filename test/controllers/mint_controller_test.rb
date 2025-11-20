461e22473628c4cdebd5399f3b4b34396f8dc351
Gregory Saumier-Finch <gregory@culturecreates.com>
2025-11-20 11:44:34 -0500

    Test ruleset to run tests before merging

--- test/controllers/mint_controller_test.rb
+++ test/controllers/mint_controller_test.rb
@@ -4,7 +4,7 @@ class MintControllerTest < ActionDispatch::IntegrationTest
   
   test "should redirect to root with missing externalUri" do
     get mint_preview_url, params: { classToMint: 'SomeClass' }
-    assert_equal 'Missing a required param. Required list: [:externalUri]', flash[:alert]
+    assert_equal 'rrrrrMissing a required param. Required list: [:externalUri]', flash[:alert]
   end
   
 end
