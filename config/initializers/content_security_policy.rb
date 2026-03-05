eb614916f35e59d52ec87bd9245a90b2321bc36f
Gregory Saumier-Finch <gregory@culturecreates.com>
2026-03-04 22:49:39 -0500

    extract origins from all endpoints

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -6,6 +6,7 @@
 
 Rails.application.configure do
   config.content_security_policy do |policy|
+    require 'uri'
     policy.default_src :self, :https
     policy.font_src    :self, :https, :data
     policy.img_src     :self, :https, :data
@@ -15,15 +16,29 @@ Rails.application.configure do
                        "https://ga.jspm.io",
                        "https://www.googletagmanager.com"
     policy.style_src   :self, :https, :unsafe_inline
-    policy.connect_src :self, 
-                        Rails.application.config.graph_api_endpoint,
-                        Rails.application.config.artsdata_recon_endpoint,
-                        Rails.application.config.artsdata_recon_endpoint_v0,
-                        Rails.application.config.artsdata_mint_endpoint,
-                        Rails.application.config.artsdata_link_endpoint,
-                        Rails.application.config.artsdata_databus_endpoint,
-                        Rails.application.config.artsdata_maintenance_endpoint,
-                        "https://www.google-analytics.com"
+
+    # Helper to extract origin from a URL string
+    def origin_of(url)
+      return nil unless url
+      uri = URI.parse(url)
+      return nil unless uri.scheme && uri.host
+      port = uri.port && ![80, 443].include?(uri.port) ? ":#{uri.port}" : ""
+      "#{uri.scheme}://#{uri.host}#{port}"
+    rescue URI::InvalidURIError
+      nil
+    end
+
+    connect_src_origins = [
+      Rails.application.config.graph_api_endpoint,
+      Rails.application.config.artsdata_recon_endpoint,
+      Rails.application.config.artsdata_recon_endpoint_v0,
+      Rails.application.config.artsdata_mint_endpoint,
+      Rails.application.config.artsdata_link_endpoint,
+      Rails.application.config.artsdata_databus_endpoint,
+      Rails.application.config.artsdata_maintenance_endpoint
+    ].map { |url| origin_of(url) }.compact.uniq
+
+    policy.connect_src :self, *connect_src_origins, "https://www.google-analytics.com"
     # Specify URI for violation reports
     # policy.report_uri "/csp-violation-report-endpoint"
   end
