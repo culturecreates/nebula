0632146b326d19ac92efd1dd7f67a65c2d2381b9
Gregory Saumier-Finch <gregory@culturecreates.com>
2026-03-09 13:16:19 -0400

    history view first pass

--- app/controllers/history_controller.rb
+++ app/controllers/history_controller.rb
@@ -1,13 +1,16 @@
 class HistoryController < ApplicationController
 
   def show
-    @uri = params[:uri]
-    @entity = Entity.new(entity_uri: @uri)
-    @history = @entity.history
-    if @history.nil?
-      flash[:alert] = "Entity has no history"
-      redirect_to root_path
-      return
-    end
+    @uri = params[:uri] + (if params[:uri].ends_with?("_dataset") then "" else "_dataset" end)
+    redirect_to entity_path(uri: @uri)
+ 
+    # TODO: implement history page
+    # @entity = Entity.new(entity_uri: @uri)
+    # @history = @entity.history
+    # if @history.nil?
+    #   flash[:alert] = "Entity has no history"
+    #   redirect_to root_path
+    #   return
+    # end
   end
 end
