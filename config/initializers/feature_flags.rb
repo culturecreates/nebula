aa0426056a4b412627a102d8f3853e70f29e8929
Gregory Saumier-Finch <gregory@culturecreates.com>
2026-03-12 20:10:32 -0400

    Maintenance on

--- config/initializers/feature_flags.rb
+++ config/initializers/feature_flags.rb
@@ -2,7 +2,7 @@
 Rails.application.config.feature_minting_enabled = false # minting includes reconciliation and linking sameAs.
 Rails.application.config.feature_refresh_entity_enabled = false
 Rails.application.config.feature_delete_entity_enabled = false
-Rails.application.config.feature_maintenance_mode_enabled = false # set to false for normal operation
+Rails.application.config.feature_maintenance_mode_enabled = true # set to false for normal operation
 Rails.application.config.announcement_enabled = false
 Rails.application.config.announcement_message = "News flash! <a href=/query/show?sparql=reconcile_controller/graphs_with_agents_to_reconcile>Batch Reconcile</a> for People, Organizations and Places is now available!".html_safe
 Rails.application.config.announcement_start_time = 1758728904
